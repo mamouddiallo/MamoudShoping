0dc7275dce95ce89a687244fee5e073494a4375e
MAYACOM AGENCY <mayacomagency@iMac-de-Cheick.local>
2024-07-09 15:44:24 +0000

    Modelisation et structure des donnees

--- ECOM/settings.py
+++ ECOM/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'UTC'
 
@@ -117,11 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,55 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def shop(request, *args, **kwargs):
-    """ Vue des produits"""
-    context = {}
-    return render(request, "../templates/shop/index.html", context)
+    """ Vue principale"""
+    produits = Produit.objects.all()
+    context = {
+        'produits':produits
+    }
+    return render(request, 'shop/index.html', context)
+
+def panier(request, *args, **kwargs):
+    """panier"""
+    if request.user.is_authenticated:
+        client = request.user.client
+        commande, created = Commande.objects.get_or_create(client=client, complete=False)
+        articles = commande.commandearticle_set.all()
+    else:
+        articles = []
+        commande = {
+            'get_panier_total':0,
+            'get_panier_article':0
+        }
+    
+    context = {
+        'articles' : articles,
+        'commande':commande
+    }
+    
+    return render(request, 'shop/panier.html', context)
+
+
+def commande(request, *args, **kwargs):
+    """" la commande"""
+    
+    if request.user.is_authenticated:
+        client = request.user.client
+        commande, created = Commande.objects.get_or_create(client=client, complete=False)
+        articles = commande.commandearticle_set.all()
+    else:
+        articles = []
+        commande = {
+            'get_panier_total':0,
+            'get_panier_article':0
+        }
+    
+    context = {
+        'articles' : articles,
+        'commande':commande
+    }
+    
+    return render(request, 'shop/commande.html', context)
+
